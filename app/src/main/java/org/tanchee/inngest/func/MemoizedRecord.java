a2908df3ceeffec1ff2a2c24e1e71972bf12ce97
Niko Ware <wrnkt@icloud.com>
2026-04-14 18:03:17 -0400

    add Memoized{Record,State}.java

---
+++ app/src/main/java/org/tanchee/inngest/func/MemoizedRecord.java
@@ -0,0 +1,5 @@
+package org.tanchee.inngest.func;
+
+import java.util.HashMap;
+
+public class MemoizedRecord extends HashMap<String, Object> {}
---
+++ app/src/main/java/org/tanchee/inngest/func/MemoizedState.java
@@ -0,0 +1,6 @@
+package org.tanchee.inngest.func;
+
+import java.util.HashMap;
+
+public class MemoizedState extends HashMap<String, MemoizedRecord> {
+}
