50f3b0c2a8bba2121b553adf964786aec5b31e04
Niko Ware <wrnkt@icloud.com>
2026-04-14 17:58:32 -0400

    switch to array

--- app/src/main/java/org/tanchee/inngest/SendEventPayload.java
+++ app/src/main/java/org/tanchee/inngest/SendEventPayload.java
@@ -1,18 +1,30 @@
 package org.tanchee.inngest;
 
-import java.util.List;
+import java.util.Arrays;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class SendEventPayload {
     @JsonProperty("event_ids")
-    private final List<String> eventIds;
+    private final String[] eventIds;
 
-    public SendEventPayload(List<String> eventIds) {
+    public SendEventPayload(String... eventIds) {
         this.eventIds = eventIds;
     }
 
-    public List<String> getEventIds() {
+    public String[] getEventIds() {
         return eventIds;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof SendEventPayload that)) return false;
+        return Arrays.equals(eventIds, that.eventIds);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(eventIds);
     }
 }
